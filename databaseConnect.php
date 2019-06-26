2dc92f7f8a9121f7ec8290e0a765ca913c5f2fc0
Dimitri <devebeme@gmail.com>
2019-06-26 08:07:56 -0400

    adding more php into the database file, will comment the code next

--- databaseConnect.php
+++ databaseConnect.php
@@ -15,17 +15,28 @@ $password = "password";
 // creating connection to database, passing in variables with authentication within (servername,username,password);
 $conn = new mysqli($servername,$username,$password);
 // code above will create a new mysqli object that has servername,username,password within
-
+// $conn is now an object and not just a variable containing data. it not has inherited object methods established in php
 // check connection
 if ($conn->connect_error) {
   die("connection failed: " . $conn->connect_error);
 }
-// this code will try the authentication ( conn ) against the connect_error object
+//
 
 // this will be a variable that holds sql, so we shall name thee sql
     // this is querying the following: columns: id, firstname, lastname, FROM Table: MyGuest
 $sql = "SELECT id, firstname, lastname FROM MyGuest";
 $result = $conn->query($sql);
+// the above contains my auth data, and will
+
+f ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close()
 
 
 
